7719c97f1d7c6a066300fe12fd2066dde87f3c28
Andrey Polubotko <andrey.polubotko@gmail.com>
2021-06-26 23:01:07 +0300

    HW2 is completed.Main task
    first version
    
    
    next version
    
    
    all tests is passed
    
    
    fix names
    
    
    remove unused
    
    
    HW2 is completed. Main task
    
    
    HW2 is completed.Main task
    
    
    fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var buf strings.Builder
+	var prevChar rune
+
+	for i, char := range s {
+		if unicode.IsDigit(char) {
+			if i == 0 || (unicode.IsDigit(prevChar)) {
+				return "", ErrInvalidString
+			}
+			if unicode.IsPunct(prevChar) {
+				buf.WriteRune(char)
+				prevChar = char
+				continue
+			}
+			n, _ := strconv.Atoi(string(char))
+			repeat := strings.Repeat(string(prevChar), n)
+			buf.WriteString(repeat)
+		}
+		if unicode.IsLetter(char) {
+			if unicode.IsLetter(prevChar) {
+				buf.WriteRune(prevChar)
+			}
+			if i == len(s)-1 {
+				buf.WriteRune(char)
+			}
+		}
+		if unicode.IsPunct(char) {
+			if unicode.IsLetter(prevChar) {
+				buf.WriteRune(prevChar)
+			}
+		}
+		prevChar = char
+	}
+
+	return buf.String(), nil
 }
