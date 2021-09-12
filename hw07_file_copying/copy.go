30e0e44ba499adc0295594f49a7300ee050ac943
Andrey Polubotko <andrey.polubotko@stoloto.ru>
2021-09-12 18:42:28 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
+)
+
+const (
+	bufferSize = 16
 )
 
 var (
@@ -10,6 +20,64 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var limitCounter, size int64
+	fileSrc, err := os.OpenFile(fromPath, os.O_RDWR, 0755)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer fileSrc.Close()
+	fileDst, err := os.OpenFile(toPath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer fileDst.Close()
+
+	sourceStat, err := fileSrc.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	if sourceStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit > 0 && limit <= sourceStat.Size() {
+		size = limit
+	} else {
+		size = sourceStat.Size() - offset
+	}
+
+	bar := pb.New(int(size)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 20)
+	bar.ShowPercent = true
+
+	data := make([]byte, bufferSize)
+
+	_, err = fileSrc.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	bar.Start()
+	for {
+		n, err := fileSrc.Read(data)
+
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		limitCounter += int64(n)
+		if limit > 0 && limitCounter > limit {
+			l := limit % bufferSize
+			fileDst.Write(data[:l])
+			bar.Add(int(l))
+			break
+		}
+		bar.Add(n)
+		if _, err := fileDst.Write(data[:n]); err != nil {
+			fmt.Printf("Write error: %v", err)
+			break
+		}
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	bar.FinishPrint("Done!")
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,52 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name        string
+		args        args
+		compareFile string
+	}{
+		{name: "limit 0", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 0, limit: 0}, compareFile: "testdata/out_offset0_limit0.txt"},
+		{name: "limit 10", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 0, limit: 10}, compareFile: "testdata/out_offset0_limit10.txt"},
+		{name: "limit 1000", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 0, limit: 1000}, compareFile: "testdata/out_offset0_limit1000.txt"},
+		{name: "limit 10000", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 0, limit: 10000}, compareFile: "testdata/out_offset0_limit10000.txt"},
+		{name: "offset 100 and limit 1000", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 100, limit: 1000}, compareFile: "testdata/out_offset100_limit1000.txt"},
+		{name: "offset 6000 and limit 1000", args: args{fromPath: "testdata/input.txt", toPath: "", offset: 6000, limit: 1000}, compareFile: "testdata/out_offset6000_limit1000.txt"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var dstFile *os.File
+			var err error
+			if len(tt.args.toPath) == 0 {
+				dstFile, err = os.CreateTemp("", "")
+				assert.NoError(t, err)
+			} else {
+				dstFile, err = os.OpenFile(tt.args.toPath, os.O_RDWR, 0755)
+				assert.NoError(t, err)
+			}
+			err = Copy(tt.args.fromPath, dstFile.Name(), tt.args.offset, tt.args.limit)
+			if assert.NoError(t, err) {
+				dstStat, err := dstFile.Stat()
+				assert.NoError(t, err)
+				cmpFile, err := os.OpenFile(tt.compareFile, os.O_RDWR, 0755)
+				assert.NoError(t, err)
+				cmpStat, err := cmpFile.Stat()
+				assert.NoError(t, err)
+				assert.Equal(t, dstStat.Size(), cmpStat.Size())
+			}
+		})
+	}
 }
